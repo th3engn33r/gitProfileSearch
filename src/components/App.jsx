873fc10f1a9aa3d2e73a77506816afbad099ebd9
engineer <shayan121@hotmail.com>
2017-02-09 16:51:59 +1100

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			username: 'bradtraversy',
+			userData: [],
+			userRepos: [],
+			perPage: 10
+		}
+	}
+
+	// Get user data from github
+	getUserData(){
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'?client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret,
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({userData: data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				this.setState({username: null});
+				alert(err);
+			}.bind(this)
+		});
+	}
+
+	// Get user repos
+	getUserRepos(){
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({userRepos: data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				this.setState({username: null});
+				alert(err);
+			}.bind(this)
+		});
+	}
+
+	handleFormSubmit(username){
+		this.setState({username: username}, function(){
+			this.getUserData();
+			this.getUserRepos();
+		});
+	}
+
+	componentDidMount(){
+		this.getUserData();
+		this.getUserRepos();
+	}
+
+	render(){
+		return(
+			<div>
+				<Search onFormSubmit = {this.handleFormSubmit.bind(this)} />
+				<Profile {...this.state} />
+			</div>
+		)
+	}
+}
+
+App.propTypes = {
+	clientId: React.PropTypes.string,
+	clientSecret: React.PropTypes.string
+};
+App.defaultProps ={
+	clientId: 'aa5d4acff1f7e803e232',
+	clientSecret: 'f71e7464794bb3434032cdd2a24a1284fea8eb9c'
+}
+
+export default App
